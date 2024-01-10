1072f2f76111e8d0bc28fb356ddf115aefaacdb4
bsaglimb <bsaglimb>
2024-01-10 17:49:06 -0500

    added connection file with code

---
+++ db/connection.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').congifg();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+    dialect: 'mysql', 
+    host: 'localhost',
+    port: '8889',
+  });
+
+module.exports = sequelize;
