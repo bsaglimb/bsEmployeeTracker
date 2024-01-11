4c00809107d45989d50f12f63e3fb1016173ea0b
bsaglimb <bsaglimb>
2024-01-11 04:10:42 -0500

    added option to add employee and job role in server.js

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const inquirer = require("inquirer");
 
 const sql = require('./db/query_lib');
 
-// adds a department
+// add a department
 const newDepartment = async () =>{
     const department = await inquirer.prompt ([
+
         {
             type: 'input',
             name: 'name',
@@ -24,3 +25,96 @@ const newDepartment = async () =>{
     chooseRequest();
 };
 
+// add an employee
+const newEmployee = async () =>{
+    
+    
+    const employee = await inquirer.prompt ([
+
+        {
+        type: 'input',
+        name: 'first_name',
+        message: 'What is the employees first name?',
+        validate: (first_name) =>{
+            if (first_name && isNaN(first_name)){
+                return true;
+            } else {
+                console.log('Please enter a name!')
+                return false;
+            }
+        },
+    },
+
+    {
+        type: 'list',
+        name: 'role_id',
+        message: 'What is the employees role?',
+        choices: employeeRoles,
+        loop: false,
+    },
+    {
+        type: 'list',
+        name: 'manager_id',
+        message: 'Who is the employees manager?',
+        choices: mgmt,
+        loop: false,
+    }
+]);
+
+    await sql.addEmployee(employee);
+
+    chooseRequest();
+};
+
+// add a role
+
+const newRole = async () =>{
+const jobRoles = await sql.getDepartments(); //correct this line accordingly to get the correct data from the database
+
+    const role = await inquirer.prompt ([
+
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the name of the job role?',
+            validate: (title) =>{
+                if (title){
+                    return true;
+                } else {
+                    console.log('Please enter a job role!');
+                    return false;
+                }
+            },
+        },
+
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'What is the salary for this job role?',
+            validate: (salary) =>{
+                if (salary && !isNaN(salary)){
+                    return true;
+                } else {
+                    console.log('Please enter a salary!');
+                    return false;
+                }
+            },
+        },
+
+        {
+            type: 'list',
+            name: 'department_id',
+            message: 'What department is this job role in?',
+            choices: jobRoles,
+            loop: false,
+        }
+    ]);
+    await sql.addRole(role);
+
+    chooseRequest();
+}
+
+
+// BONUS objectives
+
+// Delete an employee
