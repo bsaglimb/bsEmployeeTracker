35758390073a7f5d9a3b46a1b3aa0c664d7088e1
bsaglimb <bsaglimb>
2024-01-11 03:31:34 -0500

    created the schema

---
+++ db/schema.sql
@@ -0,0 +1,35 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+-- Departments table
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (department_name)
+);
+
+-- Roles table
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) 
+    REFERENCES department(id)
+);
+
+-- Employees table
+CREATE TABLE employee (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT REFERENCES employee(id),
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+);
