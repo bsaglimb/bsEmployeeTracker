63494a8591177b038e4bc71d78d58387a69000f7
bsaglimb <bsaglimb>
2024-01-11 23:54:32 -0500

    finished adding methods to the class in the query_lib.js

--- db/query_lib.js
+++ db/query_lib.js
@@ -1,10 +1,12 @@
 const db = require('./connection');
 
+// Create a class for the database queries
 class DbQuery {
     constructor(db){
         this.db = db;
     }
 
+// Add methods to the class
     addDepartment(data){
         return this.db
             .promise()
@@ -59,6 +61,59 @@ class DbQuery {
         .query('SELECT e.first_name AS "first name", e.last_name AS "last name", d.department_name AS department FROM employee e INNER JOIN role r ON e.role_id = r.id INNER JOIN department d ON r.department_id = d.id WHERE d.id = ?', values);
     }
 
-    
+    getEmployeeByMgrId(data){
+        const values = [data.manager_id];
+        return this.db
+        .promise()
+        .query(
+            'SELECT e.first_name AS "first name", e.last_name AS "last name", CONCAT(mgmt.first_name, " ", mgmt.last_name) AS Manager FROM employee e INNER JOIN employee mgmt ON e manager_id = mgmt.id WHERE e.manager_id = ?', values);
+    }
+
+    getBudgetByDepartment(data){
+        return this.db
+            .promise()
+            .query(
+                'SELECT d.department_name AS department, SUM(r.salary) AS budget FROM role r INNER JOIN department d ON r.department_id = d.id GROUP BY department',);
+    }
+     
+    getRoles(){
+        return this.db
+        .promise()
+        .query('SELECT r.titles AS title, r.salary, d.department_name AS department FROM role r LEFT JOIN department d ON r.department_id = d.id ORDER BY department, r.id ASC');
+    }
+
+    getRoleIds(){
+        return this.db
+        .promise()
+        .query('SELECT * FROM role');
+    }
+
+    getEmployee(){
+        return this.db
+        .promise()
+        .query('SELECT e.id AS "employee_Id", e.first_name AS "first_name", e.last_name AS "last_name", department.department_name AS department, role.salary AS salary, role.title AS role, CONCAT(mgmt.first_name, " ", mgmt.last_name) AS manager FROM employee e LEFT JOIN employee mgmt on e.manager_id = mgmt.id INNDER JOIN role ON e.role_id = role.id LEFT JOIN department ON role.department_id = department.id ORDER BY e.id;');
+    }
+
+    getEmployeeRaw(){
+        return this.db
+        .promise()
+        .query('SELECT e.id, e.first_name, e.last_name FROM employee e');
+    }
+
+    getNonManagers(){
+        return this.db
+        .promise()
+        .query('SELECT id, CONCAT(first_name, " ", last_name) AS employee name FROM employee WHERE manager_id IS NOT NULL');
+    }
+
+
+    getManagers(){
+        return this.db
+        .promise()
+        .query('SELECT id, CONCAT(first_name, " ", last_name) AS manager_name FROM employee WHERE manager_id IS NULL');
+    }
 
 }
+
+// Export the class
+module.exports = new DbQuery(db);
