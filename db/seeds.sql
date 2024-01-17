3f9f6686aab73e8fff529a5fcb57e6a4b5aa587d
bsaglimb <bsaglimb>
2024-01-16 19:14:26 -0500

    added employees to seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -19,3 +19,16 @@ VALUES ("Database Manager", 90000, 1),
        ("FullStack Lead", 100000, 4),
        ("FullStack Representative", 85000, 4);
 
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Sally", "Smith", 1, NULL),
+       ("Allie", "Miller", 2, 1),
+       ("Tim", "Allen", 3, 1),
+       ("Barbara", "Hicks", 4, NULL),
+       ("Lisa", "Jones", 5, 4),
+       ("John", "Henderson", 6, 4),
+       ("Sarah", "Anderson", 7, NULL),
+       ("Kaitlin", "Waver", 8, 7),
+       ("Bill", "Paulson", 9, 7),
+       ("Tori", "Thomson", 10, NULL),
+       ("Jeryy", "Peterson", 11, 10),
+       ("Larry", "David", 12, 10);
